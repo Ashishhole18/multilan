1900b2b29d2eb31e00f96fbacd1b10e1757f0dad
Ashish Holee <ashish.holey56@gmail.com>
2020-12-06 21:27:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'multilanguage';
+  title = 'DeccanSportClub';
+  constructor(public translate:TranslateService)
+  {
+
+    translate.addLangs(['en','hi','mr','ml']);
+    translate.setDefaultLang('en');
+    const browserLang=translate.getBrowserLang();
+    translate.use(browserLang.match(/en|hi|ml|mr/) ? browserLang : 'en');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {TranslateModule,TranslateLoader } from '@ngx-translate/core'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClient,HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { NguiMapModule} from '@ngui/map';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyDZERCrCxsi_i4-WVNxclrOE3e6f_CNZBg'}),
+    TranslateModule.forRoot({
+      loader:{
+
+        provide:TranslateLoader,
+        useFactory:HttpLoadFactory,
+        deps:[HttpClient]
+      }
+
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoadFactory(http:HttpClient)
+{
+  return new TranslateHttpLoader(http);
+}
